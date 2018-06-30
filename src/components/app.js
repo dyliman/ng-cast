6b2e5920abb0e3ea135f3c92ce2248213bf1bf03
dyliman <dyyliman@gmail.com>
2018-06-29 18:11:38 -0700

    passed all tests for video list and video list entry

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,19 @@
 angular.module('video-player')
 
-.component('app', {
-  // TODO
-});
+  .component('app', {
+    templateUrl: 'src/templates/app.html', 
+  
+    controller: function() {
+      
+      this.videos = window.exampleVideoData;
+      this.currentVideo = window.exampleVideoData[0];
+      console.log(this.videos);
+      
+      this.onClick = function(video) {
+        console.log(video);
+        this.currentVideo = video;
+        console.log(this.currentVideo);
+      };
+    }
+
+  });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,13 @@
 angular.module('video-player')
 
-.component('videoPlayer', {
-  // TODO
-});
+  .component('videoPlayer', {
+    templateUrl: 'src/templates/videoPlayer.html',
+    bindings: {
+      video: '<'
+    },
+  
+    controller: function() {
+      console.log(this.video);
+    }
+
+  });
